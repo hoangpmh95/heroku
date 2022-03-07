c102f7110c5cb932d395f2956be03ef1fa260b9d
Hoang <hoang.pmh95@gmail.com>
2022-03-07 11:06:00 +0700

    C1: Init

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AppService {
+    constructor(private configService: ConfigService) {}
+    getHello(): string {
+        const port = this.configService.get('PORT');
+        const nodeEnv = this.configService.get('NODE_ENV', 'Default');
+        return `PORT: ${port} - NODE_ENV: ${nodeEnv}`;
+    }
+}
